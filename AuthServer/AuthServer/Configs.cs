07760a58367567c394492f7129870a3517c1e25d
Raghav Sharma <raghavs@BrandonAssociates.com>
2019-11-18 23:43:46 +0530

    First Commit for the  Auth and Protected API

---
+++ AuthServer/AuthServer/Configs.cs
@@ -0,0 +1,70 @@
+﻿using IdentityModel;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AuthServer
+{
+    public class Configs
+    {
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client> {
+            new Client {
+                ClientId = "oauthClient",
+                ClientName = "Example Client Credentials Client Application",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = new List<Secret> {
+                    new Secret("superSecretPassword".Sha256())},
+                AllowedScopes = new List<string> {"customAPI.read"}
+            }
+        };
+        }
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource> {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+            new IdentityResources.Email(),
+            new IdentityResource {
+                Name = "role",
+                UserClaims = new List<string> {"role"}
+            }
+        };
+        }
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource> {
+            new ApiResource {
+                Name = "customAPI",
+                DisplayName = "Custom API",
+                Description = "Custom API Access",
+                UserClaims = new List<string> {"role"},
+                ApiSecrets = new List<Secret> {new Secret("scopeSecret".Sha256())},
+                Scopes = new List<Scope> {
+                    new Scope("customAPI.read"),
+                    new Scope("customAPI.write")
+                }
+            }
+        };
+        }
+        public static List<TestUser> GetTestUsers()
+        {
+            return new List<TestUser> {
+            new TestUser {
+                SubjectId = "5BE86359-073C-434B-AD2D-A3932222DABE",
+                Username = "raghav",
+                Password = "sharma",
+                Claims = new List<Claim> {
+                    new Claim(JwtClaimTypes.Email, "raghavs280@gmail.com"),
+                    new Claim(JwtClaimTypes.Role, "admin")
+                }
+            }
+        };
+        }
+    }
+}
